349003675bafbb5eac498bbdc10b7fc59759564a
Alan Russell <ajrussellaudio@gmail.com>
2018-08-03 22:58:30 +0100

    reduce fire api controller

--- app/controllers/fires_controller.rb
+++ app/controllers/fires_controller.rb
@@ -1,41 +1,15 @@
-class FiresController < ApplicationController
+class FiresController < ApiController
   before_action :set_fire, only: [:show, :update, :destroy]
 
   # GET /fires
   def index
     @fires = Fire.all
-
     render json: @fires
   end
 
   # GET /fires/1
   def show
     render json: @fire
-  end
-
-  # POST /fires
-  def create
-    @fire = Fire.new(fire_params)
-
-    if @fire.save
-      render json: @fire, status: :created, location: @fire
-    else
-      render json: @fire.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /fires/1
-  def update
-    if @fire.update(fire_params)
-      render json: @fire
-    else
-      render json: @fire.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /fires/1
-  def destroy
-    @fire.destroy
   end
 
   private
