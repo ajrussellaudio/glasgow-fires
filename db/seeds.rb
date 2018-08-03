335ef4b970c5396240106b7fe32e3338097af117
Alan Russell <ajrussellaudio@gmail.com>
2018-08-03 22:56:06 +0100

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+fires = [
+  {
+    building_name: "Thai Orchid (?)",
+    street_address: "336 Argyle St",
+    latitude: 55.8589498,
+    longitude: -4.2620268,
+    date: Date.new(2018, 8, 3)
+  },
+  {
+    building_name: "Stobhill Hospital",
+    street_address: "133 Balornock Rd",
+    latitude: 55.8924115,
+    longitude: -4.218182,
+    date: Date.new(2018, 7, 31)
+  }
+]
+
+Fire.create(fires)
