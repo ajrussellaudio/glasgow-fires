f252bd3759fe1e508579d5c7d637f3d95ecb48ea
Alan Russell <ajrussellaudio@gmail.com>
2018-08-04 02:04:12 +0100

    seed db with news sources

--- app/controllers/fires_controller.rb
+++ app/controllers/fires_controller.rb
@@ -1,25 +1,16 @@
 class FiresController < ApiController
-  before_action :set_fire, only: [:show, :update, :destroy]
 
   # GET /fires
   def index
     @fires = Fire.all
-    render json: @fires.to_json(include: :news_sources)
+    render json: @fires.to_json(
+      include: {
+        news_sources: {
+          only: [:publication, :headline, :url]
+        }
+      },
+      except: [:created_at, :updated_at]
+    )
   end
-
-  # GET /fires/1
-  def show
-    render json: @fire
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_fire
-      @fire = Fire.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def fire_params
-      params.require(:fire).permit(:building_name, :street_address, :latitude, :longitude, :date)
-    end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
-fires = [
+thaiOrchid = Fire.create(
+  building_name: "Thai Orchid (?)",
+  street_address: "336 Argyle St",
+  latitude: 55.8589498,
+  longitude: -4.2620268,
+  date: Date.new(2018, 8, 3)
+)
+
+thaiOrchid.news_sources.create([
   {
-    building_name: "Thai Orchid (?)",
-    street_address: "336 Argyle St",
-    latitude: 55.8589498,
-    longitude: -4.2620268,
-    date: Date.new(2018, 8, 3)
+    publication: "Glasgow Live",
+    headline: "Fire crews rush to city centre as smoke seen billowing from Argyle Street building",
+    url: "https://www.glasgowlive.co.uk/news/glasgow-news/fire-crews-scene-smoke-seen-14988053"
   },
   {
-    building_name: "Stobhill Hospital",
-    street_address: "133 Balornock Rd",
-    latitude: 55.8924115,
-    longitude: -4.218182,
-    date: Date.new(2018, 7, 31)
+    publication: "Evening Times",
+    headline: "Popular Glasgow City Centre restaurant on fire",
+    url: "http://www.eveningtimes.co.uk/news/16397507.popular-glasgow-city-centre-restaurant-on-fire/"
   }
-]
+  ])
 
-Fire.create(fires)
+stobhill = Fire.create(
+  building_name: "Stobhill Hospital",
+  street_address: "133 Balornock Rd",
+  latitude: 55.8924115,
+  longitude: -4.218182,
+  date: Date.new(2018, 7, 31)
+)
+
+stobhill.news_sources.create([
+  {
+    publication: "Glasgow Live",
+    headline: "Major fire breaks out at former Stobhill hospital in north Glasgow",
+    url: "https://www.glasgowlive.co.uk/news/glasgow-news/major-fire-breaks-out-former-14975532"
+  },
+  {
+    publication: "Daily Record",
+    headline: "Two teenagers arrested after major Glasgow fire at derelict Stobhill Hospital",
+    url: "https://www.dailyrecord.co.uk/news/scottish-news/two-teenagers-arrested-after-major-13029247"
+  }
+])
