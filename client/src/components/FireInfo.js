55966601da4e29f9a1c1c4ffdca85bdbdbc6b24e
Alan Russell <ajrussellaudio@gmail.com>
2018-08-04 02:43:37 +0100

    tweak info popup

--- client/src/components/FireInfo.js
+++ client/src/components/FireInfo.js
@@ -2,11 +2,19 @@ import React from "react";
 import { Popup } from "react-leaflet";
 import moment from "moment";
 
-const FireInfo = ({ building_name, street_address, date }) => (
+const FireInfo = ({ building_name, street_address, date, news_sources }) => (
   <Popup>
-    <h3>{building_name}</h3>
-    <p>{street_address}</p>
-    <p>Date: {moment(date).format("ddd, Do MMM YYYY")}</p>
+    <h3>
+      {building_name}, {street_address}
+    </h3>
+    <p>{moment(date).format("ddd, Do MMM YYYY")}</p>
+    <ul>
+      {news_sources.map(({ publication, headline, url }) => (
+        <li>
+          <span>{headline}</span> | <a href={url}>{publication}</a>
+        </li>
+      ))}
+    </ul>
   </Popup>
 );
 
