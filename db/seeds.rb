f12c7ab9b978e20247d6f944f596c88efa2eeae3
Alan Russell <ajrussellaudio@gmail.com>
2018-08-05 22:55:01 +0100

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
 
+Source.delete_all
+Fire.delete_all
 Building.delete_all
 
 art_school = Building.create(
