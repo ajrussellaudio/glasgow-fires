9d923124c8c43e6f451ef522ee518998e4c3e932
Alan Russell <ajrussellaudio@gmail.com>
2018-08-08 09:17:45 +0100

    remove client folder for separate repo

--- client/src/components/NewsSource.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const NewsSource = ({ url, headline, publication }) => (
-  <p>
-    {headline} | <a href={url}>{publication}</a>
-  </p>
-);
-
-export default NewsSource;
